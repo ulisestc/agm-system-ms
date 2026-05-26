1b1989e44a81ea7793639ad6dde884e58d23b7e2
Johan Yuri Martínez García <johanyuri24@gmail.com>
2026-05-26 12:01:26 -0600

    feat: mantener público el punto final de salud, excluir de la autenticación JWT"

--- microservices/ms-periodos-materias/config/urls.py
+++ microservices/ms-periodos-materias/config/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
 
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import AllowAny
 
+@authentication_classes([])
+@permission_classes([AllowAny])
 def health_check(_request):
     return JsonResponse({"success": True, "message": "ms-periodos-materias is running"})
 
